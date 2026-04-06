318b7f08bdcb3200d5b96585c485bb6b8a4a8bce
Edneia Ribeiro <edneia.ribe@gmail.com>
2026-04-06 20:18:37 -0300

    feat: criar adapter para Toast messages

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,14 @@ import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
-import { Bounce, ToastContainer } from 'react-toastify';
+import { MessagesContainer } from './components/MessagesContainer';
 
 export function App() {
   return (
     <TaskContextProvider>
-      <Home />
-
-      <ToastContainer
-        position='top-center'
-        autoClose={10000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={true}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme='light'
-        transition={Bounce}
-      />
+      <MessagesContainer>
+        <Home />
+      </MessagesContainer>
     </TaskContextProvider>
   );
 }
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,7 +9,7 @@ import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 import { Tips } from '../Tips';
-import { toast } from 'react-toastify';
+import { showMessage } from '../../adapters/showMessage';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -21,13 +21,14 @@ export function MainForm() {
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    showMessage.dismiss();
+
     if (taskNameInput.current === null) return;
 
     const taskName = taskNameInput.current.value.trim();
 
     if (!taskName) {
-      // alert('Por favor, insira o nome da tarefa.');
-      toast.warn('Por favor, insira o nome da tarefa.');
+      showMessage.warn('Digite o nome da tarefa.');
       return;
     }
 
@@ -42,9 +43,14 @@ export function MainForm() {
     };
 
     dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
+
+    showMessage.success(`Tarefa "${taskName}" iniciada!`);
   }
 
   function handleInterruptTask() {
+    showMessage.dismiss();
+    showMessage.error('Tarefa interrompida.');
+
     dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
